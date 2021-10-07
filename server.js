3fe68507b364870dad311d2553acabb664b09d13
Muhammad Uzair Danyal <muzaird@gmail.com>
2021-10-07 15:50:49 +0500

    port updated

--- server.js
+++ server.js
@@ -27,5 +27,5 @@ app.get('/about', function(req, res) {
   res.render('pages/about');
 });
 
-app.listen(8080);
+app.listen(process.env.PORT || 8080);
 console.log('Server is listening on port 8080');
